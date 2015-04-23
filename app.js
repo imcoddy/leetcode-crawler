866396cbaeb5e85a26b150edccf304afc1f8e919
Qiu Di <imcoddy@gmail.com>
2015-04-23 16:13:17 +0900

    decode problem url and get problem properties. using eval to parse code

--- app.js
+++ app.js
@@ -7,26 +7,45 @@ var baseUrl = 'https://leetcode.com';
 
 var problemUrls = [];
 superagent.get(baseUrl + '/problemset/algorithms/')
-.end(function (err, res) {
-    if (err) {
-        return console.error(err);
-    }
-    var $ = cheerio.load(res.text);
-    $('#problemList tbody a').each(function (idx, element) {
-        var $element = $(element);
-        var href = url.resolve(baseUrl, $element.attr('href'));
-        problemUrls.push(href);
-    });
+    .end(function(err, res) {
+        if (err) {
+            return console.error(err);
+        }
+        var $ = cheerio.load(res.text);
+        $('#problemList tbody a').each(function(idx, element) {
+            var $element = $(element);
+            var href = url.resolve(baseUrl, $element.attr('href'));
+            problemUrls.push(href);
+        });
 
-    var fetchUrl = function (url, callback) {
-        callback(null, url);
-    };
+        var fetchUrl = function(url, callback) {
+            superagent.get(url)
+                .end(function(err, res) {
+                    if (err) {
+                        return console.error(err);
+                    }
 
-    //problemUrls = problemUrls.slice(0, 3); // TODO use for test. comment out this line
-    async.mapLimit(problemUrls, 3, function (url, callback) {
-        fetchUrl(url, callback);
-    }, function (err, result) {
-        console.log('final:');
-        console.log(result);
+                    $ = cheerio.load(res.text);
+                    var result = {};
+                    result.href = url;
+                    result.tags = [];
+                    result.title = $(".question-title h3").text();
+                    result.content = $(".question-content").text();
+                    $('#tags').next().find('a').each(function(i, e) {
+                        result.tags.push($(e).text());
+                    });
+                    var code = $('#ajaxform .row .col-md-12').attr('ng-init');
+                    code = code.substring(5,code.length -9);
+                    result.code = eval(code);
+                    callback(null, result);
+                });
+        };
+
+        problemUrls = problemUrls.slice(0, 3); // TODO use for test. comment out this line
+        async.mapLimit(problemUrls, 3, function(url, callback) {
+            fetchUrl(url, callback);
+        }, function(err, result) {
+            console.log('final:');
+            console.log(result);
+        });
     });
-});
